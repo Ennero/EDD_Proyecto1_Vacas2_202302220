e047635a795aaced979267592ebfe2718025618e
Enner Mendizabal <159963896+Ennero@users.noreply.github.com>
2024-12-04 22:36:18 -0600

    Comenzando mal :)

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,27 +1,61 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
-{
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++)
-    {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+string usuarioAdmin,contraAdmin,depaAdmin,empreAdmin;
+
+usuarioAdmin="Administrador"
+
+
+
+
+void ingreso(){ //void para el ingreso de los datos
+    //Declaración de algunas variables que usaré
+    string usuario,contra,depa,empre;
+
+    system("cls");
+    cout<<"---------------RENTA DE ACTIVOS---------------"<<endl;
+    cout<<"Iniciar Sesión"<<endl;
+
+    //Ingreso de todos los datos
+    cout<<"Ingrese Usuario:"<<endl;
+    cin>>usuario;
+    cout<<"Ingrese Contraseña:"<<endl;
+    cin>>contra;
+    cout<<"Ingrese Departamento"<<endl;
+    cin>>depa;
+    cout<<"Ingrese Empresa:"<<endl;
+    cin>>empre;
+
+
+}
+
+int main(){ //El ciclo principal en donde voy a colocar todo
+
+    bool inicio=true;
+    cout<<"---------------RENTA DE ACTIVOS---------------"<<endl;
+    while (inicio){ //Ciclo principal en donde se inicia
+        int opcionInicio; //Declaro la variable antes :)
+        cout<<"Ingrese un número "<<endl; //Mensajitos
+        cout<<"1. Iniciar Sesión"<<endl;
+        cout<<"2. Salir"<<endl;
+        cin >> opcionInicio;//Ingreso de la variables solicitada
+
+        switch (opcionInicio){ //El switchcase
+        case 1: //Caso para inicializar el
+            ingreso();
+            inicio=false;
+        case 2:
+            inicio=false;
+            cout<<"Saliendo..."<<endl;
+        default:
+            cout<<"Opcion incorrecta"<<endl;
+        }
     }
+
+
+
 
     return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
